8ded0b1fd8a62a1426933fdd5ea9d2174236d41d
Ariana Promessi <ariana@promessi.com>
2015-09-04 17:09:13 -0700

    created node class and circular linked list - working on 'kill_every'

--- josephus.py
+++ josephus.py
@@ -1,17 +1,57 @@
+class Node(object):
+    """Doubly-linked node."""
+
+    def __init__(self, data, prev=None, next=None):
+        self.data = data
+        self.prev = prev
+        self.next = next
+
+    def remove(self):
+        self.prev.next = self.next
+        self.next.prev = self.prev
+
+    def __repr__(self):
+        return "<Node prev=%s data=%s next=%s>" % (self.prev.data, self.data, self.next.data)
+
+
 def find_survivor(num_people, kill_every):
-    """Given num_people in a circle, kill everyone [kill_every]th person, return survivor.
+    # """Given num_people in a circle, kill everyone [kill_every]th person, return survivor.
 
-        >>> find_survivor(4, 2)
-        1
+    #     >>> find_survivor(4, 2)
+    #     1
 
-        >>> find_survivor(41, 3)
-        31
+    #     >>> find_survivor(41, 3)
+    #     31
 
-    As a sanity case, if never skip anyone, the last person will be our survivor:
+    # As a sanity case, if never skip anyone, the last person will be our survivor:
 
-        >>> find_survivor(10, 1)
-        10
-    """
+    #     >>> find_survivor(10, 1)
+    #     10
+    # """
+
+    head = Node(1)
+    prev = head
+    for num in range(2, num_people+1):
+        new_node = Node(num, prev)
+        prev.next = new_node
+        prev = new_node
+    head.prev = new_node
+
+    current = head
+    while current.next or current.prev:
+        for i in range(kill_every):
+            current = current.next
+            print current, "current"
+        kill = current
+        print kill, "kill"
+        current = current.next
+        kill.remove()
+        
+    return current
+
+find_survivor(10, 3)
+# find_survivor(41, 3)
+
 
 
 if __name__ == '__main__':

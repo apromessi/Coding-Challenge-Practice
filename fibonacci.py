4ca5d9e4498596bd1a8d7d07d8286db120598093
Ariana Promessi <ariana@promessi.com>
2015-09-07 16:11:09 -0700

    completed two fibonacci solutions

--- fibonacci.py
+++ fibonacci.py
@@ -1,13 +1,50 @@
 # different ways of finding the nth fibonacci term
+# [1, 1, 2, 3, 5, 8, 13, 21]
 
 
 def fib(n):
-    pass
+    """
+    >>> fib(5)
+    5
+
+    >>> fib(1)
+    1
+
+    >>> fib(8)
+    21
+
+    """
+
+    if n <= 2:
+        return 1
+    else:
+        a, b = 1, 1
+        n = n - 2
+        while n:
+            a, b = b, a+b
+            n -= 1
+        return b
 
 
 def fib_recursive(n):
-    pass
+    """
+    >>> fib_recursive(5)
+    5
+
+    >>> fib_recursive(1)
+    1
+
+    >>> fib_recursive(8)
+    21
+
+    """
+    if n <= 2:
+        return 1
+    num = fib(n-1) + fib(n-2)
+    return num
 
 
-def fib_memoization(n):
-    pass
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED. NICE WORK!\n"

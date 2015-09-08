934387d48793d0fe3446d33415caf36e0cb427f7
Ariana Promessi <ariana@promessi.com>
2015-09-08 12:06:39 -0700

    added doctests to reverse

--- reverse.py
+++ reverse.py
@@ -1,10 +1,34 @@
 def reverse_list(a_list):
+    """
+    >>> reverse_list(['a', 'b', 'c', 'd', 'e'])
+    ['e', 'd', 'c', 'b', 'a']
+
+    >>> reverse_list([5])
+    [5]
+    """
+
     pass
 
 
 def reverse_list_slicing(a_list):
+    """
+    >>> reverse_list(['a', 'b', 'c', 'd', 'e'])
+    ['e', 'd', 'c', 'b', 'a']
+
+    >>> reverse_list([5])
+    [5]
+    """
+
     pass
 
 
 def reverse_string(string):
+    """
+    >>> reverse_string('abcde')
+    'edcba'
+
+    >>> reverse_string('Hello World!')
+    '!dlroW olleH'
+    """
+
     pass

cd7b195663a63ace5ebe2b1d33d709ecd2342e85
Ariana Promessi <ariana@promessi.com>
2015-09-13 14:31:45 -0700

    built method for reversing linked list

--- linkedlistpractice.py
+++ linkedlistpractice.py
@@ -13,22 +13,31 @@ class LinkedList(object):
         current = self.head
         for data in data_points[1:]:
             current.next = Node(data)
-            current = current.next # may get attribute error
-
-
+            current = current.next
 
     def print_ll(self):
         current = self.head
-        while current.next:
+        while current:
             print current.data
             current = current.next
 
     def reverse(self):
-        current = self.head
-        previous = None
-        while current.next:
-            previous = current
-            current = current.next
+        previous = self.head
+        current = previous.next
+        previous.next = None
+        next = current.next
+        while next:
             current.next = previous
+            previous = current
+            current = next
+            next = current.next
+        current.next = previous
         self.head = current
-
+        
+linked = LinkedList()
+linked.create_ll([1, 2, 3, 4, 5, 6, 7])
+print "Print forwards list"
+linked.print_ll()
+linked.reverse()
+print "Print reversed list"
+linked.print_ll()

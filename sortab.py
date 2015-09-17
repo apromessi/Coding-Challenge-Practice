50518e9bd07379fca596069358210437a2b9e2dd
Ariana Promessi <ariana@promessi.com>
2015-09-16 18:54:10 -0700

    solved sortab - a portion of merge sort

--- sortab.py
+++ sortab.py
@@ -16,9 +16,29 @@ def sort_ab(a, b):
 
     Check:
 
-        >>> sort_ab([1, 3, 5, 7], [2, 6, 8, 10])
-        [1, 2, 3, 5, 6, 7, 8, 10]
+        >>> sort_ab([1, 3, 5, 7, 8, 11], [2, 6, 8, 10])
+        [1, 2, 3, 5, 6, 7, 8, 8, 10, 11]
     """
+
+    ab = []
+    if a and b:
+        a_i = 0
+        b_i = 0
+        while a_i < len(a) and b_i < len(b):
+            if a[a_i] < b[b_i]:
+                ab.append(a[a_i])
+                a_i += 1
+            elif a[a_i] >= b[b_i]:
+                ab.append(b[b_i])
+                b_i += 1
+        a = a[a_i:]
+        b = b[b_i:]
+
+    if a:
+        ab.extend(a)
+    if b:
+        ab.extend(b)
+    return ab
 
 
 if __name__ == '__main__':
